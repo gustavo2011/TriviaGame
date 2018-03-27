b8997a6fdfc98673ca00dc46e51fe620d82b5cc2
gustavo2011 <gustavo.jauregui2011@gmail.com>
2018-03-26 22:14:48 -0700

    gave start button functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,8 @@
 console.log("this is working");
+//start game with hidden divs
+$(".game_content").hide();
+$(".final_stats").hide();
 
-var triviaQuestions = [{
-    question1 = "Which team is known in Spanish as “El Tri”?",
-    answers = ["Holand" , "Mexico" , "Spain" , "England"],
-    correctAnswer = 1
-
-}]
-
-function showQuestion(triviaQuestions) {
-    $("#questions").html(triviaQuestions.question1);
-}
-
-function countDown () {
-    seconds= 20;
-    $("#timer").html("<h4>Your time remaining : " + seconds + "</h4>");
-    answered: true;
-    time = setInterval(1000);
-}
+$("#startButton").on("click" , function(){
+    console.log("this button works");
+})
